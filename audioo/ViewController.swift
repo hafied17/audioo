59b0cc11eff04f8f7dba730084d4d6310b161ea2
hafied17 <hafiedmadrid6@gmail.com>
2020-11-02 11:58:15 +0700

    selesai

--- audioo/ViewController.swift
+++ audioo/ViewController.swift
@@ -7,14 +7,39 @@
 //
 
 import UIKit
+import InstantSearchVoiceOverlay
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, VoiceOverlayDelegate {
 
+    let voiceOverlay = VoiceOverlayController()
+    @IBOutlet var myButton: UIButton!
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        myButton.backgroundColor = .systemRed
+        myButton.setTitleColor(.white, for: .normal)
     }
-
-
+    @IBAction func didTapButton(){
+        voiceOverlay.delegate = self
+        voiceOverlay.settings.autoStart = false
+        voiceOverlay.settings.autoStop = true
+        voiceOverlay.settings.autoStopTimeout = 10
+    
+        
+        voiceOverlay.start(on: self, textHandler: { text, final, _ in
+            
+            if final {
+                print("Final text: \(text)")
+            }else{
+                print("In progress: \(text)")
+            }
+            
+        }, errorHandler: { error in
+            
+        })
+    }
+    func recording(text: String?, final: Bool?, error: Error?){
+        
+    }
 }
 
